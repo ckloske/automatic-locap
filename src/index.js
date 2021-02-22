b25c269b50f70879af584156aa97bde415afdcad
ckloske <ckloske@gmail.com>
2021-02-22 16:36:41 +0100

    fix minNotional issue

--- src/index.js
+++ src/index.js
@@ -23,12 +23,12 @@ let isLong = false;
 let isLocked = false;
 let buyPrice = 0;
 let buyQuantity = 0;
-let availableAmount = STARTING_AMOUNT;
+let availableAmount = parseFloat(STARTING_AMOUNT);
 let lastOrderID = 0;
 
 const log = (message) => {
   const now = new Date();
-  console.log(chalk.blueBright(now), message);
+  console.log(chalk.blueBright(now), `${y(PAIR)}: `, message);
 }
 
 const tradeCallback = (data) => {
@@ -37,19 +37,19 @@ const tradeCallback = (data) => {
   if (lastOrderID === orderId) {
     let direction = side === 'BUY' ? -1 : +1;
     if (orderStatus == 'REJECTED') {
-      log(`${y(PAIR)}: Order ${orderId} failed. Reason: ${data.r}`);
+      log(`Order ${orderId} failed. Reason: ${data.r}`);
       isLocked = false;
     } else if (orderStatus === 'CANCELED') {
-      log(`${y(PAIR)}: Order ${orderId} canceled manually.`)
+      log(`Order ${orderId} canceled manually`)
       buyPrice = 0;
       buyQuantity = 0;
       isLong = false;
       isLocked = false;
     } else if (orderStatus == 'PARTIALLY_FILLED') {
-      log(`${y(PAIR)}: Order ${orderId} partially executed. ${side} ${orderType} ${quantity} @ ${price}. Available: ${availableAmount}.`);
+      log(`Order ${orderId} partially executed. ${side} ${orderType} ${quantity} @ ${price}. Available: ${availableAmount}`);
     } else if (orderStatus == 'FILLED') {
-      availableAmount += direction * (quantity * price);
-      log(`${y(PAIR)}: Order ${orderId} fully executed. ${side} ${orderType} ${quantity} @ ${price}. Available: ${availableAmount}.`);
+      availableAmount = availableAmount + (direction * (quantity * price));
+      log(`Order ${orderId} fully executed. ${side} ${orderType} ${quantity} @ ${price}. Available: ${availableAmount}`);
       if (side === 'SELL') {
         log(`Profit: ${(((quantity * price) / STARTING_AMOUNT) - 1) * 100}%`);
       }
@@ -57,8 +57,11 @@ const tradeCallback = (data) => {
       buyQuantity = side === 'BUY' ? quantity : 0;
       isLong = side === 'BUY';
       isLocked = false;
+    } else if (orderStatus == 'NEW') {
+      lastOrderID = res.orderId;
+      log(`Order ${res.orderId} in book`);
     } else {
-      log(`${y(PAIR)}: Ignored status ${orderStatus}`)
+      log(`Ignored status ${orderStatus}`)
     }
   }
 }
@@ -78,10 +81,24 @@ function autoTrade(pair, interval, chart) {
   const loBand = binance.roundTicks(sampleMean - sampleStd, FILTERS.tickSize);
   const quantity = binance.roundStep(availableAmount / lastClosePrice, FILTERS.stepSize);
 
-  log([buyPrice, buyQuantity]);
+  if (
+    quantity < FILTERS.minQty
+    || quantity > FILTERS.maxQty
+    || quantity * lastClosePrice < FILTERS.minNotional
+  ) {
+    let message = quantity * lastClosePrice < FILTERS.minNotional ?
+      `Minimum notional value: ${quantity * lastClosePrice} < ${FILTERS.minNotional}`
+      : `Quantity out of bounds: ${quantity}`
+    log(message);
+    log(`Shutting down`);
+    process.exit(22)
+  }
 
   if (
-    lastClosePrice < loBand
+    quantity >= FILTERS.minQty
+    && quantity <= FILTERS.maxQty
+    && quantity * lastClosePrice >= FILTERS.minNotional
+    && lastClosePrice < loBand
     && !isLong
     && !isLocked
   ) {
@@ -89,20 +106,22 @@ function autoTrade(pair, interval, chart) {
     binance.buy(pair, quantity, lastClosePrice)
       .then((res) => {
         log(res);
-        log(`${y(PAIR)}: Order ${res.orderId} created. ${res.side} ${res.type} ${res.origQty} @ ${res.price}.`);
+        log(`Order ${res.orderId} created. ${res.side} ${res.type} ${res.origQty} @ ${res.price}`);
         if (res.status === 'NEW') {
           lastOrderID = res.orderId;
-          log(`${y(PAIR)}: Order ${res.orderId} in book.`);
+          log(`Order ${res.orderId} in book`);
         } else if (res.status === 'FILLED') {
-          availableAmount -= res.executedQty * res.price;
+          availableAmount = availableAmount - res.executedQty * res.price;
           isLong = true;
           isLocked = false;
           buyPrice = res.price;
           buyQuantity = res.executedQty;
           lastOrderID = 0;
-          log(`${y(PAIR)}: Order ${res.orderId} fully executed.`);
+          log(`Order ${res.orderId} fully executed. Available: ${availableAmount}`);
+        } else if (orderStatus == 'PARTIALLY_FILLED') {
+          log(`Order ${orderId} partially executed. ${side} ${orderType} ${quantity} @ ${price}. Available: ${availableAmount}`);
         } else {
-          console.warn(`${y(PAIR)}: Unexpected status ${res.status}`)
+          console.warn(`Ignored status ${res.status}`)
           isLocked = false;
         }
 
@@ -117,31 +136,33 @@ function autoTrade(pair, interval, chart) {
     binance.sell(pair, buyQuantity, lastClosePrice)
       .then((res) => {
         log(res);
-        log(`${y(PAIR)}: Order ${res.orderId} created. ${res.side} ${res.type} ${res.origQty} @ ${res.price}.`);
+        log(`Order ${res.orderId} created. ${res.side} ${res.type} ${res.origQty} @ ${res.price}`);
         if (res.status === 'NEW') {
           lastOrderID = res.orderId;
-          log(`${y(PAIR)}: Order ${res.orderId} in book.`);
+          log(`Order ${res.orderId} in book`);
         } else if (res.status === 'FILLED') {
-          availableAmount += res.executedQty * res.price;
+          availableAmount = availableAmount + res.executedQty * res.price;
           isLong = false;
           isLocked = false;
           buyPrice = 0;
           buyQuantity = 0;
           lastOrderID = 0;
-          log(`${y(PAIR)}: Order ${res.orderId} fully executed.`);
+          log(`Order ${res.orderId} fully executed. Available: ${availableAmount}`);
+        } else if (orderStatus == 'PARTIALLY_FILLED') {
+          log(`Order ${orderId} partially executed. ${side} ${orderType} ${quantity} @ ${price}. Available: ${availableAmount}`);
         } else {
-          console.warn(`${y(PAIR)}: Unexpected status ${res.status}`)
+          log(`Unexpected status ${res.status}`)
           isLocked = false;
         }
-
       })
       .catch(log);
 
   } else {
     let message = isLong ? 'Waiting for opportunity to sell' : 'Waiting for opportunity to buy';
     message = isLocked ? 'Waiting order to be executed' : message;
-    // console.info(`${y(PAIR)}: ${interval}. ${message}. Close: ${lastClosePrice}, Hi/Lo: ${hiBand}/${loBand}`);
+    // console.info(`${interval}. ${message}. Close: ${lastClosePrice}, Hi/Lo: ${hiBand}/${loBand}`);
   }
+
 };
 
 binance.exchangeInfo(function (error, data) {
@@ -167,7 +188,13 @@ binance.exchangeInfo(function (error, data) {
   }
   global.filters = minimums;
 
-  log(`${y(PAIR)} started with amount ${STARTING_AMOUNT}, interval ${INTERVAL}, stdv-factr ${STD_FACTOR}, ${SAMPLES} samples,  minimum profit ${SELL_LIMIT_PCT}.`)
+  log(`
+  ${y(PAIR)} started
+  with amount ${STARTING_AMOUNT}, 
+  interval ${INTERVAL}, 
+  stdv-factr ${STD_FACTOR}, 
+  ${SAMPLES} samples, 
+  minimum profit ${SELL_LIMIT_PCT}%`)
 
   binance.websockets.userData(() => null, tradeCallback)
   binance.websockets.chart(PAIR, INTERVAL, autoTrade);
